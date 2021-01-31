345bf91e0b10e17dec73c7e7e0e6b74bea69f159
MarcTM <marctorresmartinez@gmail.com>
2021-01-31 22:57:56 +0100

    Prova

---
+++ .github/actions/notification/index.js
@@ -0,0 +1,25 @@
+const core = require("@actions/core");
+const sendmail = require('sendmail')();
+
+const destination = core.getInput("destination");
+ 
+sendmail({
+    from: 'marctorresmartinez@gmail.com',
+    to: destination,
+    subject: 'Resultado del workflow ejecutado',
+    html: `
+        <p>Se ha realizado un push en la rama githubActions_improvement que
+        ha provocado la ejecución del workflow Bingo_Workflow con los
+        siguientes resultados</p>
+
+        <ul>
+            <li>syntax_check_job: resultado asociada</li>
+            <li>test_execution_job: resultado asociada</li>
+            <li>build_statics_job: resultado asociada</li>
+            <li>deploy_job: resultado asociada</li>
+        </ul>
+    `
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
+});
