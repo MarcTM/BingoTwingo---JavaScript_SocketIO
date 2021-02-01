d25221158639381e3c45afe5270ec730d86a8cb0
MarcTM <marctorresmartinez@gmail.com>
2021-02-01 12:50:56 +0100

    .

--- .github/actions/notification/index.js
+++ .github/actions/notification/index.js
@@ -3,25 +3,26 @@ const sendmail = require('sendmail')();
 
 const destination = core.getInput("destination");
 const syntax_check_job = core.getInput("syntax_check_job");
-const test_execution_job = core.getInput("test_execution_job");
+const job = core.getInput("job2");
 const build_statics_job = core.getInput("build_statics_job");
 const deploy_job = core.getInput("deploy_job");
 
  
-sendmail({
-    from: 'marctorresmartinez@gmail.com',
-    to: destination,
-    subject: 'Resultado del workflow ejecutado',
-    html: `
-        <p>Se ha realizado un push en la rama githubActions_improvement que
-        ha provocado la ejecución del workflow Bingo_Workflow con los
-        siguientes resultados</p>
+ console.log(job2)
+// sendmail({
+//     from: 'marctorresmartinez@gmail.com',
+//     to: destination,
+//     subject: 'Resultado del workflow ejecutado',
+//     html: `
+//         <p>Se ha realizado un push en la rama githubActions_improvement que
+//         ha provocado la ejecución del workflow Bingo_Workflow con los
+//         siguientes resultados</p>
 
-        <ul>
-            <li>test_execution_job: ${{ test_execution_job }}</li>
-        </ul>
-    `
-  }, function(err, reply) {
-    console.log(err && err.stack);
-    console.dir(reply);
-});
+//         <ul>
+//             <li>test_execution_job: ${{ job2 }}</li>
+//         </ul>
+//     `
+//   }, function(err, reply) {
+//     console.log(err && err.stack);
+//     console.dir(reply);
+// });
