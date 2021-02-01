43bc75b8a86ec129c4e91a98ee6bc0224e296d80
MarcTM <marctorresmartinez@gmail.com>
2021-02-01 13:27:53 +0100

    .

--- .github/actions/notification/index.js
+++ .github/actions/notification/index.js
@@ -2,26 +2,32 @@ const core = require("@actions/core");
 const sendmail = require('sendmail')();
 
 const destination = core.getInput("destination");
+const password = core.getInput("email_password");
 // const syntax_check_job = core.getInput("syntax_check_job");
 // const test_execution_job = core.getInput("test_execution_job");
 // const build_statics_job = core.getInput("build_statics_job");
 // const deploy_job = core.getInput("deploy_job");
 
- 
-sendmail({
-    from: 'marctorresmartinez@gmail.com',
-    to: 'marctorresmartinez@gmail.com',
-    subject: 'Resultado del workflow ejecutado',
-    html: `
-        <p>Se ha realizado un push en la rama githubActions_improvement que
-        ha provocado la ejecución del workflow Bingo_Workflow con los
-        siguientes resultados</p>
+const nodemailer = require("nodemailer");
 
-        <ul>
-            <li>test_execution_job: hoalholah</li>
-        </ul>
-    `
-  }, function(err, reply) {
-    console.log(err && err.stack);
-    console.dir(reply);
-});
+async function main() {
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'marctorresmartinez@gmail.com',
+      pass: password,
+    },
+  });
+
+
+  let info = await transporter.sendMail({
+    from: '"Marc Torres 👻" <marctorresmartinez@.com>',
+    to: "marctorresmartinez@gmail.com",
+    subject: "Hello ✔",
+    text: "Hello world?",
+    html: "<b>Hello world?</b>",
+  });
+}
+
+main().catch(console.error); 
