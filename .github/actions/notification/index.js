814504b1cf01e787d7d1a38fe8d5a940dde63927
MarcTM <marctorresmartinez@gmail.com>
2021-02-01 18:40:54 +0100

    iyu

--- .github/actions/notification/index.js
+++ .github/actions/notification/index.js
@@ -9,10 +9,10 @@ let test_execution_job;
 let build_statics_job;
 let deploy_job;
 
-core.getInput("job1") == 'success' ? syntax_check_job = 'success' : syntax_check_job = "error";
+// core.getInput("job1") == 'success' ? syntax_check_job = 'success' : syntax_check_job = "error";
 core.getInput("job2") == 'success' ? test_execution_job = 'success' : test_execution_job = "error";
-core.getInput("job3") == 'success' ? build_statics_job = 'success' : build_statics_job = "error";
-core.getInput("job4") == 'success' ? deploy_job = 'success' : deploy_job = "error";
+// core.getInput("job3") == 'success' ? build_statics_job = 'success' : build_statics_job = "error";
+// core.getInput("job4") == 'success' ? deploy_job = 'success' : deploy_job = "error";
 
 
 async function main() {
@@ -29,13 +29,10 @@ async function main() {
     from: '"Marc Torres 👻" <marctorresmartinez@.com>',
     to: destination,
     subject: "Se ha realizado un push en la rama githubActions_improvement que ha provocado la ejecución del workflow Bingo_Workflow con los siguientes resultados: ✔",
-    text: "Hello world?",
+    text: test_execution_job,
     html: `
         <ul>
-            <li>syntax_check_job: ${{ syntax_check_job }}</li>
             <li>test_execution_job: ${{ test_execution_job }}</li>
-            <li>build_statics_job: ${{ build_statics_job }}</li>
-            <li>deploy_job: ${{ deploy_job }}</li>
         </ul>
     `,
   });
